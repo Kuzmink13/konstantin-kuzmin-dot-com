ce8dc0ac8e52781f8ec6364b6d0bd6510b6b9c23
Konstantin Kuzmin <konstantinkuzmin9@gmail.com>
2023-03-19 22:05:13 -0500

    deps and fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Atkinson Hyperlegible', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [require('daisyui')],
-  daisyui: {
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: '',
-    darkTheme: 'dark',
-  },
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/typography'),
+  ],
 }
